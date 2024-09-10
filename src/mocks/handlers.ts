d6d474e2117c0cbb71badcc10eef6b7e24db36fd
TinsFox <fox@tinsfox.com>
2024-09-10 18:36:39 +0800

    feat: log

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,8 +3,15 @@ import { http, HttpResponse } from "msw"
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
   http.get("/api/user", (request) => {
-    console.log("request: ", request)
     // ...and respond to them using this JSON response.
+    const { cookies } = request
+    console.log("cookies: ", cookies)
+    // if (!cookies.token) {
+    //   return HttpResponse.json(null)
+    // }
+    if (cookies.token !== "1234567890") {
+      throw new HttpResponse(null, { status: 401 })
+    }
     return HttpResponse.json({
       id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
       firstName: "John",
