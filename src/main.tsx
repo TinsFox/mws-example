234a68e457de2555b753ec724650a3b921c3725a
TinsFox <fox@tinsfox.com>
2024-09-11 11:43:09 +0800

    use loader to fetch

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,41 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) {
-    return
-  }
-
   const { worker } = await import("./mocks/browser")
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
   return worker.start()
 }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
+    loader: async () => {
+      const data = await fetch("/api/user", {
+        credentials: "include",
+      })
+      console.log("data: ", await data.text())
 
+      return {
+        data: 1,
+      }
+    },
+  },
+])
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   )
 })
